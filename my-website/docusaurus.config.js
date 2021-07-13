795e9b2d5ebab722b3417975635fce5b3d38745d
koushar-tasneem <86586763+koushar-tasneem@users.noreply.github.com>
2021-07-13 20:40:58 +0530

    Update docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -38,7 +38,7 @@ module.exports = {
 
 
             {
-                label: "Gith",
+                label: "Git",
                 href: "https://github.com/puppeteer/puppeteer",
                 position: "right",
             },
